5da28263375c5acbea25d06125e960a75a94f267
athoillahvip <athoillahvip@gmail.com>
2021-10-07 17:35:46 +0800

    perbaiki functions

--- functions.php
+++ functions.php
@@ -1,3 +1,14 @@
 <?php
 //koneksi ke databasse
 $conn = mysqli_connect("localhost", "root", "", "php_login");
+
+function query($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
