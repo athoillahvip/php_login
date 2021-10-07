5fd08db923093d24f54a5eb7b5799d62198025d3
athoillahvip <athoillahvip@gmail.com>
2021-10-07 17:58:46 +0800

    perbaikan file registrasi

--- registrasi.php
+++ registrasi.php
@@ -12,11 +12,11 @@
     <title>Registrasi</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="http://localhost/php_login/vendor/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="http://localhost/php_login/vendor/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -81,14 +81,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="http://localhost/php_login/vendor/vendor/jquery/jquery.min.js"></script>
+    <script src="http://localhost/php_login/vendor/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="http://localhost/php_login/vendor/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="http://localhost/php_login/vendor/js/sb-admin-2.min.js"></script>
 
 </body>
 
